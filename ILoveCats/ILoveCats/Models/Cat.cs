086be07c2af65c89ab7050fa0437ac39d3d913e0
alternia42 <alternia42@gmail.com>
2020-02-14 01:03:36 +0300

    add context

--- ILoveCats/ILoveCats/Models/Cat.cs
+++ ILoveCats/ILoveCats/Models/Cat.cs
@@ -1,11 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace ILoveCats.Models
 {
     public class Cat
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public bool Sex { get; set; }
+        public string Breed { get; set; }
+        public string Colour { get; set; }
+        public string Coat { get; set; }
+        
+
     }
 }
---
+++ ILoveCats/ILoveCats/Models/CatContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ILoveCats.Models
+{
+    public class CatContext : DbContext
+    {
+        public CatContext(DbContextOptions<CatContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CatContext> Cats { get; set; }
+    }
+}
